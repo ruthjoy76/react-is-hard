c4439d5a65aaed6e508df279ee2d71135dd5c976
ruthjoy76 <ruthjoy76@gmail.com>
2023-05-22 20:59:21 +0800

    Update

--- src/services/notes.js
+++ src/services/notes.js
@@ -4,3 +4,22 @@ const baseUrl = "http://localhost:3001/notes";
 function getAllNotes() {
   return axios.get(baseUrl);
 }
+
+function createNote(newNote) {
+  return axios.post(baseUrl, newNote);
+}
+
+function updateNote(id, newNote) {
+  return axios.put(`${baseUrl}/${id}`, newNote);
+}
+
+function deleteNote(id) {
+  return axios.delete(`${baseUrl}/${id}`);
+}
+
+export default {
+  getAllNotes,
+  createNote,
+  updateNote,
+  deleteNote,
+};
